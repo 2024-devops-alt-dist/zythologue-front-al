ea822f71ce894212551fc0618415f25ef1b9f006
Adrien <adrienleyval4@gmail.com>
2025-01-14 14:40:52 +0100

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
 import BeerPage from './pages/BeersPage';
+import Header from './components/Header';
 
 
 function App() {
 
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/beers" element={<BeerPage />} />
       </Routes>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,47 @@
+import { Link, NavLink } from "react-router"
+
 function Header() {
     return (
         <header>
-            <nav className="flex justify-between items-center py-5 px-5 border text-grey-950">
+            <nav className="sticky z-10 top-0 w-100 flex justify-between items-center py-5 px-5 border text-gray-950 bg-white">
                 <div className="flex items-center">
-                    <a href="" className="flex">
-                        <img src="" alt="logo zythologue" />
+                    <Link to="" className="flex justify-between gap-5">
+                        <img src="/public/zythologue_logo.jpg" alt="logo zythologue" className="max-h-10"/>
                         <span className="self-center text-xl font-bold">Zythologue</span>
-                    </a>
-                    <div className="flex items-center gap-10 border ms-5">
+                    </Link>
+                    <div className="flex items-center gap-10 ms-10">
                         <div>
-                            <a href="">Accueil</a>
+                            <NavLink to="/">Accueil</NavLink>
                         </div>
                         <div>
-                            <a href="">Bières</a>
+                            <NavLink to="/beers">Bières</NavLink>
                         </div>
                         <div>
-                            <a href="">Brasseries</a>
+                            <NavLink to="/breweries">Brasseries</NavLink>
                         </div>
                     </div>
                 </div>
 
-                <div className="flex">
+                <div className="flex justify-center gap-5  ">
                     <div>
-                        <a href="">Se connecter</a>
+                        <NavLink to="/login">
+                            <button className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-full px-6 font-medium text-gray-950 duration-500  border-gray-950">
+                                <div className="translate-x-0 opacity-100 transition group-hover:-translate-x-[150%] group-hover:opacity-0">Se connecter</div>
+                                <div className="absolute translate-x-[150%] opacity-0 transition group-hover:translate-x-0 group-hover:opacity-100 text-gray-950 ">                                    
+                                    <img src="/src/assets/icons/arrow_right.svg" alt="" className="text-gray-950"/>
+                                </div>
+                            </button>
+                        </NavLink>
                     </div>
                     <div>
-                        <a href="">S'inscrire</a>
+                        <NavLink to="/signup">
+                            <button className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-full px-6 font-medium text-gray-950 duration-500  group-hover:border ">
+                                <div className="translate-x-0 opacity-100 transition group-hover:-translate-x-[150%] group-hover:opacity-0">S'inscrire</div>
+                                <div className="absolute translate-x-[150%] opacity-0 transition group-hover:translate-x-0 group-hover:opacity-100 text-gray-950 ">                                    
+                                    <img src="/src/assets/icons/arrow_right.svg" alt="" className="text-gray-950"/>
+                                </div>
+                            </button>
+                        </NavLink>
                     </div>
                 </div>
             </nav>
