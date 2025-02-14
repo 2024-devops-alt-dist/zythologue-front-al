bb26e7830fe159c60e16a6e34a37c71d70776163
Adrien <adrienleyval4@gmail.com>
2025-02-14 10:18:54 +0100

    feat: add mobile menu animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,50 +11,53 @@ function Navbar() {
     }
 
     return (
-            <nav className="sticky z-20 top-0 w-full h-24 flex justify-between items-center py-5 px-5 text-white bg-black">
-                <div className="w-1/5">
-                    <Link to="/" className="flex gap-5 w-full">
-                        <img src="/public/beer_logo.png" alt="logo zythologue" className="max-h-10"/>
-                        <span className="self-center text-xl font-bold text-green-600">Zythologue</span>
-                    </Link>
+            <nav className="sticky z-50 top-0 w-full h-24 flex justify-between items-center text-white bg-black">
+                <div className="z-50 top-0 w-full h-24 flex justify-between items-center p-5 text-white bg-black">
+                    <div className="w-1/5">
+                        <Link to="/" className="flex gap-5 w-full">
+                            <img src="/public/beer_logo.png" alt="logo zythologue" className="max-h-10"/>
+                            <span className="self-center text-xl font-bold text-green-600">Zythologue</span>
+                        </Link>
+                    </div>
+                    <button 
+                        className="md:hidden"
+                        onClick={displayMenu}
+                    >
+                        <img src="/src/assets/icons/burger_menu.svg" alt=""  className="w-10 h-10" />
+                    </button>
                 </div>
-                <button 
-                    className="md:hidden"
-                    onClick={displayMenu}
-                >
-                    <img src="/src/assets/icons/burger_menu.svg" alt=""  className="w-10 h-10" />
-                </button>
-                <div className={`w-full z-10 fixed p-5 left-0 bg-zinc-800 md:hidden transition-transform ${active ? 'top-24' : 'top-[-100%]'}`}>
+
+                <div className={`w-full z-40 fixed p-5 left-0 bg-zinc-800 md:hidden transition-transform top-24 ${active ? 'translate-y-0' : 'translate-y-[-150%]'}`}>
                     <div className="flex flex-col items-center justify-center gap-5 p-5">
-                        <NavLink to="/">
+                        <NavLink to="/" onClick={() => setActive(false)}>
                             {({ isActive }) => (
                                 <span className={isActive ? "text-white font-bold" : "text-gray-300 hover:text-white"} >Accueil</span>
                             )}
                         </NavLink>
 
-                        <NavLink to="/beers">
+                        <NavLink to="/beers" onClick={() => setActive(false)}>
                             {({ isActive }) => (
                                 <span className={isActive ? "text-white font-bold" : "text-gray-300 hover:text-white"} >Bières</span>
                             )}
                         </NavLink>
 
-                        <NavLink to="/breweries">
+                        <NavLink to="/breweries" onClick={() => setActive(false)}>
                             {({ isActive }) => (
                                 <span className={isActive ? "text-white font-bold" : "text-gray-300 hover:text-white"} >Brasseries</span>
                             )}
                         </NavLink>
-                        <NavLink to="/beers/create">
+                        <NavLink to="/beers/create" onClick={() => setActive(false)}>
                             {({ isActive }) => (
                                 <span className={isActive ? "text-white font-bold" : "text-gray-300 hover:text-white"} >Nouvelle bière</span>
                             )}
                         </NavLink>
                     </div>
                     <div className="flex flex-col items-center justify-center">
-                        <NavLink to="/login">
+                        <NavLink to="/login" onClick={() => setActive(false)}>
                             <ArrowButton color="white" text="Se connecter"/>
                         </NavLink>
 
-                        <NavLink to="/signup">
+                        <NavLink to="/signup" onClick={() => setActive(false)}>
                             <ArrowButton color="white" text="S'inscrire"/>
                         </NavLink>
                     </div>
